063a4d3820680aa7c6f06f961f5cf366e1c232bf
Carolyn Lou <carolyn.lou@gmail.com>
2020-12-09 15:08:47 -0500

    updated functions

--- adnisurv_powercomp_function.R
+++ adnisurv_powercomp_function.R
@@ -1,5 +1,4 @@
-#function to calc min n needed for
-powercomp_gbm_surv <- function(NSIMS, n, gam, dat, pow = 0.8, rand = T){
+powercomp_surv_adni <- function(NSIMS, n, gam, pow = 0.8, data){
   len <- length(n)
   data <- dat
   
@@ -11,7 +10,7 @@ powercomp_gbm_surv <- function(NSIMS, n, gam, dat, pow = 0.8, rand = T){
   
   cens <- rep(NA, len)
   
-  #set.seed(960113)
+  set.seed(960113)
   start<- proc.time()
   pb <- txtProgressBar(0,len*NSIMS,style=3)
   for(j in 1:len){
@@ -25,54 +24,39 @@ powercomp_gbm_surv <- function(NSIMS, n, gam, dat, pow = 0.8, rand = T){
     
     for(i in 1:NSIMS){
       #split ADNI data into 2
-      n.tx <- n[j]/2
-      
-      #to prevent tx1 and tx2 arms overlap, split tx into two parts
       tx1 <- data[sample(nrow(data),nrow(data)/2,replace=F),]
       tx2 <- suppressMessages(anti_join(data, tx1))
       
+      n.tx <- n[j]/2
+      
       #treatment arm 1. tx
       ftx.1 <- tx1[sample(nrow(tx1),n.tx,replace=F),] #"bootstrap" a future tx sample of MCI pts, tx arm 1
-      A.tx.1 <- rep(1,n.tx) #trial tx'd
-      X.tx.1 <- ftx.1$SPI
-      
-      if(rand == T){
-        Y.tx.1.p <- pmin((gam + rnorm(nrow(ftx.1), 0, gam*0.1)) * ftx.1$surv3, 36)
-      }else{
-        Y.tx.1.p <- pmin(gam*ftx.1$surv3, 36)
-      }
-      
-      cens.1.p <- 1*(Y.tx.1.p < 36) 
-      
-      Y.tx.1.ti <- ftx.1$surv3
-      cens.1.ti <- 1*(Y.tx.1.ti < 36) 
+      ftx.1 = ftx.1 %>%
+        mutate(A.tx = rep(1,n.tx),#trial tx'd
+               Y.tx.p = pmin(TimeToConv*(gam + rnorm(n.tx, 0, gam*0.1)), 36),
+               cens.p = 1*(Y.tx.p < 36),
+               Y.tx.ti = TimeToConv,
+               cens.ti = 1*(Y.tx.ti < 36))
       
       #treatment arm 2, ctl
       ftx.2 <- tx2[sample(nrow(tx2),n.tx,replace=F),] #a future tx sample of MCI pts, tx arm 2
-      A.tx.2 <- rep(0, n.tx) #trial controls
-      X.tx.2 <- ftx.2$SPI
-      
-      Y.tx.2.p <- 1*ftx.2$surv3
-      cens.2.p <- 1*(Y.tx.2.p < 36) 
-      
-      Y.tx.2.ti <- ftx.2$surv3
-      cens.2.ti <- 1*(Y.tx.2.ti < 36) 
+      ftx.2 = ftx.2 %>%
+        mutate(A.tx = rep(0,n.tx),#trial tx'd
+               Y.tx.p = TimeToConv,
+               cens.p = 1*(Y.tx.p < 36),
+               Y.tx.ti = TimeToConv,
+               cens.ti = 1*(Y.tx.ti < 36))
       
       #collecting pvals
-      Y.tx.ti <- c(Y.tx.1.ti, Y.tx.2.ti)
-      cens.ti <- c(cens.1.ti, cens.2.ti)
-      Y.tx.p <- c(Y.tx.1.p, Y.tx.2.p)
-      cens.p <- c(cens.1.p, cens.2.p)
-      X.tx <- c(X.tx.1, X.tx.2)
-      A.tx <- c(A.tx.1, A.tx.2)
+      dat.final = rbind(ftx.1, ftx.2)
       
-      pvals.nopred.p[i] <- summary(coxph(Surv(time = Y.tx.p, event = cens.p) ~ A.tx))$coef[1,5]
-      pvals.pred.p[i] <- summary(coxph(Surv(time = Y.tx.p, event = cens.p) ~ A.tx + X.tx))$coef[1,5]
+      pvals.nopred.p[i] <- summary(coxph(Surv(time = Y.tx.p, cens.p) ~ A.tx + AGE + memb, data = dat.final))$coef[1,5]
+      pvals.pred.p[i] <- summary(coxph(Surv(time = Y.tx.p, event = cens.p) ~ A.tx + SPARE.AD + AGE + memb, data = dat.final))$coef[1,5]
       
-      pvals.nopred.ti[i] <- summary(coxph(Surv(time = Y.tx.ti, event = cens.ti) ~ A.tx))$coef[1,5]
-      pvals.pred.ti[i] <- summary(coxph(Surv(time = Y.tx.ti, event = cens.ti) ~ A.tx + X.tx))$coef[1,5]
+      pvals.nopred.ti[i] <- summary(coxph(Surv(time = Y.tx.ti, event = cens.ti) ~ A.tx + AGE + memb, data = dat.final))$coef[1,5]
+      pvals.pred.ti[i] <- summary(coxph(Surv(time = Y.tx.ti, event = cens.ti) ~ A.tx + SPARE.AD + AGE + memb, data = dat.final))$coef[1,5]
       
-      cens.ti.perc[i] <- sum(cens.ti)/length(cens.ti)
+      cens.ti.perc[i] <- sum(dat.final$cens.ti)/length(dat.final$cens.ti)
       
       setTxtProgressBar(pb, (j-1)*NSIMS+i)
     }
